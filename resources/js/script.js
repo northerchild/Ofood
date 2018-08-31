0ccb4477d0724919317a57d59b0fcc6fa3cb3443
Northerchild <osarus-13@hotmail.com>
2018-08-31 17:25:05 -0500

    Se agrega el sticky navigation con su estilos y efectos

---
+++ resources/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(()=>{
+
+	/* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+	/*let waypoints = $('#handler-first').waypoint(function(direction) {
+  	notify(this.element.id + ' hit 25% from top of window')}, {
+  	offset: '25%'})*/
+})
