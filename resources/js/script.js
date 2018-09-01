0a350fb93b05e8c68869ad29763699ca669ec7e0
Northerchild <osarus-13@hotmail.com>
2018-08-31 21:59:42 -0500

    Se crea el menu hamburguesa y se le da estilo ademas que es responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,6 +61,22 @@ $(document).ready(()=>{
         offset: '50%'
     });
 
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        let nav = $('.js--main-nav');
+        let icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });
+
 	/*let waypoints = $('#handler-first').waypoint(function(direction) {
   	notify(this.element.id + ' hit 25% from top of window')}, {
   	offset: '25%'})*/
