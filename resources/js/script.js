9c42e716a79f00bf28457f16ae9d7642fa599c9c
Northerchild <osarus-13@hotmail.com>
2018-08-31 20:33:11 -0500

    Se agregan las animaciones al proyecto

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,6 +11,56 @@ $(document).ready(()=>{
       offset: '60px;'
     });
 
+    /*Scroll botons*/
+    $('.js--scroll-to-plans').click(function(){
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},2000)
+    })
+
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          let target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
 	/*let waypoints = $('#handler-first').waypoint(function(direction) {
   	notify(this.element.id + ' hit 25% from top of window')}, {
   	offset: '25%'})*/
